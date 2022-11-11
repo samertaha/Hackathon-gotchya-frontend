ac0576f7bae73b6a22a6004d44eaef7dcc9c6e6a
Samer taha <alrazy1@gmail.com>
2022-11-11 13:21:42 +0200

    added the remote url api

--- src/api/gotchyaApi.js
+++ src/api/gotchyaApi.js
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 const gotchya = axios.create({
-  baseURL: "http://3.145.5.121/api",
+  //baseURL: 'http://localhost:8001/',
+  baseURL: 'ec2-3-28-221-246.me-central-1.compute.amazonaws.com/api',
 });
 
 export default gotchya;
