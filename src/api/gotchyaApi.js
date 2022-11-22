707a5e275774c0edf1f60e02dbe98195d43f91f6
Samer taha <alrazy1@gmail.com>
2022-11-22 20:39:19 +0200

    added the/api url section

--- src/api/gotchyaApi.js
+++ src/api/gotchyaApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const gotchya = axios.create({
   //baseURL: 'http://localhost:8000/',
-  baseURL: 'https://gotchyabackend.onrender.com/',
+  baseURL: 'https://gotchyabackend.onrender.com/api',
 });
 
 export default gotchya;
