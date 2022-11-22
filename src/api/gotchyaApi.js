9ea15c3ed1703ff96229b9f1bd4d3af5fcb0cc49
Samer taha <alrazy1@gmail.com>
2022-11-22 20:37:13 +0200

    changed the API url to the onrender.com live one

--- src/api/gotchyaApi.js
+++ src/api/gotchyaApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const gotchya = axios.create({
-  baseURL: 'http://localhost:8000/',
-  //baseURL: 'ec2-3-28-221-246.me-central-1.compute.amazonaws.com/api',
+  //baseURL: 'http://localhost:8000/',
+  baseURL: 'https://gotchyabackend.onrender.com/',
 });
 
 export default gotchya;
